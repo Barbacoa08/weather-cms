cd27324041e9c46724436e7b8bc6d8d6f8e4ce8d
barbajoe <sabe12070@gmail.com>
2023-06-03 20:21:43 -0500

    HACK fix for css import issue in payload package
    https://github.com/payloadcms/next-payload/issues/34#issuecomment-1569530931

--- next.config.js
+++ next.config.js
@@ -15,8 +15,9 @@ module.exports = withPayload(nextConfig, {
   // Point to your Payload config (Required)
   configPath: path.resolve(__dirname, "./payload/payload.config.ts"),
 
+  // HACK: per -> https://github.com/payloadcms/next-payload/issues/34#issuecomment-1569530931
   // Point to custom Payload CSS (optional)
-  cssPath: path.resolve(__dirname, "./css/my-custom-payload-styles.css"),
+  // cssPath: path.resolve(__dirname, "./css/my-custom-payload-styles.css"),
 
   // Point to your exported, initialized Payload instance (optional, default shown below`)
   payloadPath: path.resolve(process.cwd(), "./payload/payloadClient.ts"),
